bed3756f8c83990cc1a6f2ab2e751ccdc0651d07
Dev Abdallah Najjar <113408625+Abdullah2004g@users.noreply.github.com>
2023-06-27 19:18:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+{
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+
+function appendNumber(number) {
+  document.getElementById('result').value += number;
+}
+
+function appendOperator(operator) {
+  document.getElementById('result').value += operator;
+}
+
+function calculate() {
+  var result = eval(document.getElementById('result').value);
+  document.getElementById('result').value = result;
+}
+}
